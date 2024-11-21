8cc70ca5b3fc3b16e35a55830c137be202e4b46f
Tech2cool <aktarvishwas8520@gmail.com>
2024-11-21 19:50:23 +0530

    s

--- src/model/demand.model.js
+++ src/model/demand.model.js
@@ -9,17 +9,62 @@ export const demandSchema = new mongoose.Schema({
   },
   floor: {
     type: Number,
+    default: null,
   },
   flatNo: {
     type: Number,
+    default: null,
   },
-  reminder: {
-    date: { type: Date },
-    dueDate: { type: Date },
-    amount: { type: String },
-    amountType: { type: String },
-  },
+  reminders: [
+    {
+      customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "customer",
+        required: true,
+      },
+      date: { type: Date, default: Date.now },
+
+      dueDate: { type: Date, default: null, required: true },
+
+      //before due date
+      payableNetAmountBeforeDueDate: { type: Number, default: null },
+      payableNetAmountAfterDueDate: { type: Number, default: null },
+      //after due date
+      payableGstAmountBeforeDueDate: { type: Number, default: null },
+      payableGstAmountAfterDueDate: { type: Number, default: null },
+
+      subject: { type: String, default: null },
+
+      refId: { type: String, default: null },
+      ref: { type: String, default: null },
+
+      body: { type: String, default: null },
+      slab: { type: String, default: null },
+      slabAmount: { type: String, default: null },
+      document: { type: String, default: null },
+
+      recievedNetAmount: { type: Number, default: null },
+      recievedGstAmount: { type: Number, default: null },
+
+      recievedNetAmountDate: { type: Date, default: null },
+      recievedGstAmountDate: { type: Date, default: null },
+
+      businessAccount: {
+        accountNo: { type: String, default: null },
+        ifsc: { type: String, default: null },
+        micr: { type: String, default: null },
+        bankName: { type: String, default: null },
+      },
+
+      govAccount: {
+        accountNo: { type: String, default: null },
+        ifsc: { type: String, default: null },
+        micr: { type: String, default: null },
+        bankName: { type: String, default: null },
+      },
+    },
+  ],
 });
 
-const demandModel = mongoose.model("demand", demandSchema, "demand");
+const demandModel = mongoose.model("demands", demandSchema, "demands");
 export default demandModel;
