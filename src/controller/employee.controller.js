6c6fa7f3f0610c192a56d3c26d5e54036e405b44
Tech2cool <aktarvishwas8520@gmail.com>
2024-12-03 12:00:58 +0530

    merge emp

--- src/controller/employee.controller.js
+++ src/controller/employee.controller.js
@@ -467,6 +467,31 @@ export const editEmployeeById = async (req, res, next) => {
   }
 };
 
+export const getReportingTo = async (req, res, next) => {
+  try {
+    const reportingToId = req.params.id;
+
+    const employees = await employeeModel
+      .find({ reportingTo: reportingToId })
+      .select("firstName lastName")
+      .populate("designation")
+      .populate({
+        path: "reportingTo",
+        select: "firstName lastName",
+        populate: [{ path: "designation" }],
+      });
+
+    // Return the list of employees
+    return res.status(200).send(
+      successRes(200, "Employees reporting to the specified ID", {
+        employees,
+      })
+    );
+  } catch (error) {
+    next(error); // Pass the error to the global error handler
+  }
+};
+
 export const deleteEmployeeById = async (req, res, next) => {
   const id = req.params.id;
   try {
