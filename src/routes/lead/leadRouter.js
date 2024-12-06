ca7d4cf1277338f8e7ba1ae502cc3291e36c955a
Tech2cool <aktarvishwas8520@gmail.com>
2024-12-06 20:42:10 +0530

    lead updates csv

--- src/routes/lead/leadRouter.js
+++ src/routes/lead/leadRouter.js
@@ -26,6 +26,16 @@ import {
 } from "../../controller/lead.controller.js";
 import { authenticateToken } from "../../middleware/auth.middleware.js";
 import { validateLeadsFields } from "../../middleware/lead.middleware.js";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import csv from "csv-parser";
+import path from "path";
+import employeeModel from "../../model/employee.model.js";
+import cpModel from "../../model/channelPartner.model.js";
+import ourProjectModel from "../../model/ourProjects.model.js";
+import leadModel from "../../model/lead.model.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const leadRouter = Router();
 
@@ -117,5 +127,128 @@ leadRouter.get(
   // authenticateToken,
   checkLeadsExists
 );
+const parseDate = (dateString) => {
+  // Split the string into day, month, year
+  const [day, month, year] = dateString.split("-").map(Number);
+
+  // Create a new Date object
+  const date = new Date(year + 2000, month - 1, day); // Adjust year and month (0-indexed)
+
+  return date;
+};
+
+leadRouter.post("/lead-updates", async (req, res) => {
+  const results = [];
+  const dataTuPush = [];
+  const csvFilePath = path.join(__dirname, "Leads_tl.csv");
+
+  const cpResp = await cpModel.find();
+  const teamLeaders = await employeeModel.find({
+    $or: [
+      { designation: "desg-senior-closing-manager" },
+      { designation: "desg-site-head" },
+    ],
+  });
+  const dataAnalyzers = await employeeModel.find({
+    designation: "desg-data-analyzer",
+  });
+
+  const projectsResp = await ourProjectModel.find({});
+
+  if (!fs.existsSync(csvFilePath)) {
+    return res.status(400).send("CSV file not found");
+  }
+  let i = 0;
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      results.push(data);
+    })
+    .on("end", async () => {
+      for (const row of results) {
+        const {
+          Leadreceivedon,
+          Name: firstName,
+          Surname: lastName,
+          Number: phoneNumber,
+          Cp,
+          TeamLeaderDate: leadAssignDate,
+          Teamleader,
+          Project,
+          Requirement,
+          taggingstatus,
+        } = row;
+        let startDate = parseDate(Leadreceivedon);
+        let cycleStartDate = parseDate(leadAssignDate);
+        let requirement = Requirement.replace(/\s+/g, "")
+          .toUpperCase()
+          ?.split(",");
+        let projects = projectsResp.find((proj) =>
+          proj.name.toLowerCase().includes(Project.split("")[0])
+        )._id;
+
+        let newTeamleader =
+          teamLeaders.find((tl) =>
+            tl.firstName
+              .toLowerCase()
+              .includes(Teamleader.split(" ")[0].toLowerCase())
+          )?._id ?? null;
+
+        let channelPartner =
+          cpResp.find((cp) =>
+            cp.firmName.toLowerCase().includes(Cp.split(" ")[0].toLowerCase())
+          )?._id ?? null;
+
+        // if (Cp != "") {
+        //   const newCpId = Cp?.replace(/\s+/g, "-").toLowerCase();
+        //   try {
+        //     const newCp = await cpModel.create({
+        //       _id: newCpId,
+        //       email: Cp?.replace(/\s+/g, "").toLowerCase() + "@gmail.com",
+        //       firmName: Cp.toLowerCase(),
+        //       password: "Evhomecp",
+        //     });
+        //     channelPartner = newCp._id;
+        //   } catch (error) {}
+        // }
+
+        let dataAnalyzer = dataAnalyzers[i]?._id ?? null;
+
+        i = i >= 1 ? 0 : 1;
+        const validTill = new Date(cycleStartDate);
+        validTill.setDate(validTill.getDate() + 15);
+        // i++;
+        dataTuPush.push({
+          firstName,
+          lastName,
+          phoneNumber: phoneNumber.replace(/\s+/g, "").toLowerCase(),
+          teamLeader: newTeamleader,
+          channelPartner,
+          dataAnalyzer,
+          requirement,
+          approvalStatus: taggingstatus,
+          approvalDate: cycleStartDate,
+          startDate,
+          cycleStartDate,
+          projects,
+          cycle: {
+            nextTeamLeader: null,
+            stage: "visit",
+            currentOrder: 1,
+            teamLeader: newTeamleader,
+            startDate: cycleStartDate,
+            validTill: validTill,
+          },
+        });
+      }
+      await leadModel.insertMany(dataTuPush);
+      // Send the results only after processing is done
+      return res.send(dataTuPush);
+    })
+    .on("error", (err) => {
+      return res.status(500).send({ error: err.message });
+    });
+});
 
 export default leadRouter;
