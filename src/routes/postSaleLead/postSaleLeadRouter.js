567ade48eedc81100925837971b1b28349066197
Tech2cool <aktarvishwas8520@gmail.com>
2024-12-18 14:23:37 +0530

    s2

--- src/routes/postSaleLead/postSaleLeadRouter.js
+++ src/routes/postSaleLead/postSaleLeadRouter.js
@@ -10,28 +10,15 @@ import {
 import { authenticateToken } from "../../middleware/auth.middleware.js";
 
 const postSaleRouter = Router();
-postSaleRouter.get("/post-sale-leads", authenticateToken, getPostSaleLeads);
-postSaleRouter.post("/post-sale-lead-add", authenticateToken, addPostSaleLead);
-postSaleRouter.post(
-  "/post-sale-lead-update/:id",
-  authenticateToken,
-  updatePostSaleLeadById
-);
+postSaleRouter.get("/post-sale-leads", getPostSaleLeads);
+postSaleRouter.post("/post-sale-lead-add", addPostSaleLead);
+postSaleRouter.post("/post-sale-lead-update/:id", updatePostSaleLeadById);
 postSaleRouter.get(
   "/post-sale-leads-for-pse/:id",
-  authenticateToken,
   getPostSaleLeadsForExecutive
 );
-postSaleRouter.get(
-  "/post-sale-lead-by-id/:flatNo",
-  authenticateToken,
-  getPostSaleLeadById
-);
+postSaleRouter.get("/post-sale-lead-by-id/:flatNo", getPostSaleLeadById);
 
-postSaleRouter.get(
-  "/post-sale-lead-by-flat",
-  authenticateToken,
-  getPostSaleLeadByFlat
-);
+postSaleRouter.get("/post-sale-lead-by-flat", getPostSaleLeadByFlat);
 
 export default postSaleRouter;
