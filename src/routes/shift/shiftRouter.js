d88b913af2d3e52bf13cbd8f92ef6a3a0ed35ddc
justina jodi <justinajodis@gmail.com>
2025-01-11 19:03:54 +0530

    shift management

--- src/routes/shift/shiftRouter.js
+++ src/routes/shift/shiftRouter.js
@@ -4,12 +4,24 @@ import {
   addShift,
   getShiftById,
   getShifts,
+  deleteShiftById,
+  editShift,
+  assignShift,
+  getAssignedEmployees,
+  addEmployeesToShift,
+  removeEmployeeFromShift,
 } from "../../controller/shift.controller.js";
 const shiftRouter = Router();
 
 shiftRouter.get("/shifts", getShifts);
 shiftRouter.post("/add-shift", addShift);
 shiftRouter.get("/shift/:id", getShiftById);
+shiftRouter.delete("/shift/:id", deleteShiftById);
+shiftRouter.put("/shift-update/:id", editShift);
+shiftRouter.post("/assign-employees-to-shift", assignShift);
 
+shiftRouter.get("/shift/:shiftId/employees", getAssignedEmployees); 
+shiftRouter.post("/shift/:shiftId/add-employees", addEmployeesToShift); 
+shiftRouter.delete("/shift/:shiftId/remove-employee/:employeeId", removeEmployeeFromShift); 
 
 export default shiftRouter;
