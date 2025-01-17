e49eac9f715d46e6edef2a156799156842d78fdd
salunkhe04 <salunkheshreya19@gmail.com>
2025-01-17 14:16:07 +0530

    small change in contest controllr

--- src/routes/event/eventRouter.js
+++ src/routes/event/eventRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addEvent, getEvent, getEventById, updateEvent } from "../../controller/event.controller.js";
+import { addEvent, deleteEvent, getEvent, getEventById, updateEvent } from "../../controller/event.controller.js";
 
 const eventRouter = Router();
 eventRouter.get("/event",getEvent);
@@ -7,6 +7,7 @@ eventRouter.get("/event",getEvent);
 eventRouter.post("/event-add",addEvent);
 eventRouter.get("/event-id/:id",getEventById);
 eventRouter.post("/update-event-id/:id",updateEvent);
+eventRouter.delete("/event-delete/:id",deleteEvent);
 
 
 export default eventRouter;
