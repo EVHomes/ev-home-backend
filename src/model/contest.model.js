6ea46797cbe4287bed8072e80b32cc6e87f35d93
Tech2cool <aktarvishwas8520@gmail.com>
2025-01-17 15:26:40 +0530

    event

--- src/model/contest.model.js
+++ src/model/contest.model.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
-export const contestSchema = new mongoose.Schema({
-
-  firstName: { type: String, required: true, },
-  lastName: { type: String, required: true, },
-  phoneNumber: { type: String, required: true, default: null },
-  photoUrl:{
-    type:[String],
-    default:null,
-  }  ,
-  event:{
-    type : String,
-    ref: "event"
-  }
-});
-
+export const contestSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, default: null },
+    phoneNumber: { type: String, required: true, default: null },
+    photoUrl: {
+      type: [String],
+      default: null,
+    },
+    event: {
+      type: String,
+      ref: "event",
+    },
+  },
+  { timestamps: true }
+);
 
 // Create the model
 const contestModel = mongoose.model("Contest", contestSchema, "Contests");
