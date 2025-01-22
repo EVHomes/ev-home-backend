29f5f2fb24c7326ce408f293593e9649762ac6f3
Tech2cool <aktarvishwas8520@gmail.com>
2025-01-22 23:16:42 +0530

    ss

--- src/controller/transport.controller.js
+++ src/controller/transport.controller.js
@@ -1,5 +1,6 @@
 import { errorRes, successRes } from "../model/response.js";
 import TransportModel from "../model/transport.model.js";
+import vehicleModel from "../model/vehicle.model.js";
 import { tansportPopulateOptions } from "../utils/constant.js";
 
 export const getTransports = async (req, res) => {
@@ -22,6 +23,7 @@ export const addTransport = async (req, res) => {
     clientPhone,
     manager,
     smanagerList,
+    vehicle,
   } = req.body;
   try {
     console.log(req.body);
@@ -34,10 +36,52 @@ export const addTransport = async (req, res) => {
       startDate: new Date(startDate),
       pickupLocation: "679105383f865aa5d732512b",
     });
-
+    // await vehicleModel.findByIdAndUpdate(vehicle, { status: true });
     const resp2 = await TransportModel.findById(resp._id).populate(
       tansportPopulateOptions
     );
+
+    return res.send(successRes(200, "Added Transport", { data: resp2 }));
+  } catch (error) {
+    return res.send(errorRes(500, `${error}`));
+  }
+};
+
+export const approveTransport = async (req, res) => {
+  const { status } = req.body;
+  const id = req.params.id;
+  try {
+    console.log(req.body);
+    // if (!id) return res.send(errorRes(401, "id is required"));
+
+    // await vehicleModel.findByIdAndUpdate(vehicle, { status: true });
+    const resp2 = await TransportModel.findByIdAndUpdate(id, {
+      stage: status.toLowerCase() === "approved" ? "ontheway" : "rejected",
+      approvalStatus: status,
+    }).populate(tansportPopulateOptions);
+
+    if (status.toLowerCase() === "approved") {
+      await vehicleModel.findByIdAndUpdate(resp2.vehicle._id, { status: true });
+    }
+
+    return res.send(successRes(200, "Added Transport", { data: resp2 }));
+  } catch (error) {
+    return res.send(errorRes(500, `${error}`));
+  }
+};
+
+export const completedTransport = async (req, res) => {
+  const id = req.params.id;
+  try {
+    // if (!id) return res.send(errorRes(401, "id is required"));
+
+    // await vehicleModel.findByIdAndUpdate(vehicle, { status: true });
+    const resp2 = await TransportModel.findByIdAndUpdate(id, {
+      stage: "completed",
+      jurneyStatus: "completed",
+    }).populate(tansportPopulateOptions);
+
+    await vehicleModel.findByIdAndUpdate(resp2.vehicle._id, { status: false });
 
     return res.send(successRes(200, "Added Transport", { data: resp2 }));
   } catch (error) {
--- src/routes/transport/transportRouter.js
+++ src/routes/transport/transportRouter.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import {
   addTransport,
+  approveTransport,
+  completedTransport,
   getTransports,
 } from "../../controller/transport.controller.js";
 const transPortRouter = Router();
@@ -14,6 +16,18 @@ transPortRouter.get(
   "/get-transport",
   // authenticateToken,
   getTransports
+);
+
+transPortRouter.post(
+  "/approve-transport/:id",
+  // auth,
+  approveTransport
+);
+
+transPortRouter.post(
+  "/transport-complete/:id",
+  // authenticateToken,
+  completedTransport
 );
 
 export default transPortRouter;
