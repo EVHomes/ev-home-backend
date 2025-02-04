92810045fa4edf70ad5a4dcc3a0a290f921ead2b
salunkhe04 <salunkheshreya19@gmail.com>
2025-02-04 13:48:15 +0530

    all data

--- src/routes/lead/leadRouter.js
+++ src/routes/lead/leadRouter.js
@@ -36,6 +36,7 @@ import {
   getLeadTeamLeaderReportingToGraph,
   triggerCycleChangeFunction,
   getAssignedToSalesManger,
+  getAllData,
 } from "../../controller/lead.controller.js";
 import { authenticateToken } from "../../middleware/auth.middleware.js";
 import { validateLeadsFields } from "../../middleware/lead.middleware.js";
@@ -62,7 +63,8 @@ const __dirname = path.dirname(__filename);
 
 const leadRouter = Router();
 
-leadRouter.get("/leads", authenticateToken, getAllLeads);
+leadRouter.get("/leads", /*authenticationToken*/  getAllLeads);
+leadRouter.get("/leads-data", /*authenticationToken*/  getAllData);
 leadRouter.get(
   "/leads-team-leader/:id",
   // authenticateToken,
